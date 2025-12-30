39affa6f51b4950236de408beb46f9f91f584acc
Nguyen Huu Thuan <thuannguyen488@gmail.com>
2025-12-30 20:33:04 +0700

    up bài tập ngày 11

---
+++ NGAY11/BT11_4.py
@@ -0,0 +1,10 @@
+# Bài tập 4: 
+# Viết hàm kiểm tra xem ký tự nhập vào có phải là ký tự số không.
+#  Nếu là ký tự số hàm trả về true, ngược lại trả về false.
+def kiemtra(ch):
+    if ch.isdigit():
+        return true
+    else:
+        return false
+kt=input("nhập vào ký tự:")
+print("kết quả kiểm tra",kiemtra(kt))
